3690212e812d279a38c4fa884a747476d47bc8d9
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-03-05 14:45:47 +0200

    Add second test - not passing

--- test/trueTackleSuccessRate_test.js
+++ test/trueTackleSuccessRate_test.js
@@ -4,13 +4,28 @@ const Calculate = require('../metrics/trueTackleSuccessRate')
 
 describe ('Calculate', () => {
   
-  const { statsTackles } = stats();
+  const { statsMinutesPlayed, statsTackles, statsDuelsTotal, statsDuelsWon, foulsCommitted } = stats();
 
   describe('#getTacklesSuccessful', () => {
     it('returns the tackles made', () => {
       actual = Calculate.getTacklesSuccessful(statsTackles);
 
       expected = [30, 40, 18, 57];
+
+      assert.deepEqual(actual, expected);
+    })
+  });
+  
+  describe('#getTrueTackleSuccessRate', () => {
+    it('returns a list of the names of players and the respective tackle success rate', () => {
+
+      actual = Calculate.getTrueTackleSuccessRate(statsTackles);
+
+      expected = [{"name": "Thiago Alcântara", "trueTackleSuccessRate": 1.5 },
+        {"name": "İ. Gündoğan", "trueTackleSuccessRate": 0.8888888888888888 },
+        {"name": "Aaron Wan-Bissaka", "trueTackleSuccessRate": 0.461538461538462},
+        {"name": "Rodri", "trueTackleSuccessRate": 0.81428}
+      ];
 
       assert.deepEqual(actual, expected);
     })
